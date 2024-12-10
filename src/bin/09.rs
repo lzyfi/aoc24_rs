85872b0f8c5b5c97e2cba042950cde823d4a01f7
lzyfi <68154354+lzyfi@users.noreply.github.com>
2024-12-10 12:23:11 +0200

    day 9, slow but works

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -30,6 +30,41 @@ fn parse(input: &str) -> Vec<Block> {
     temp
 }
 
+fn parse2(input: &str) -> Vec<isize> {
+    let mut temp = vec![];
+    let mut id = 0;
+
+    let mut data = true;
+
+    for char in input.chars() {
+        if let Some(n) = char.to_digit(10) {
+            if data {
+                temp.append(&mut vec![id; n as usize]);
+                id += 1;
+            } else {
+                temp.append(&mut vec![-1; n as usize]);
+            }
+            data = !data;
+        } else {
+            println!("Invalid {}", char as u8);
+            panic!();
+        }
+    }
+
+    temp
+}
+
+fn checksum2(input: &[isize]) -> usize {
+    input
+        .iter()
+        .enumerate()
+        .map(|(i, n)| match n {
+            -1 => 0,
+            id => i * *id as usize,
+        })
+        .sum()
+}
+
 pub fn part_one(input: &str) -> Option<usize> {
     let layout = parse(input);
     let mut temp = layout.clone();
@@ -77,8 +112,38 @@ pub fn part_one(input: &str) -> Option<usize> {
     )
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let layout = parse2(input);
+    let mut temp = layout.clone();
+
+    let max = *layout.iter().max().unwrap();
+
+    let mut inds = vec![0; max as usize + 1];
+    inds[0] = 0;
+    let mut count = 0;
+    for (ind, val) in temp.iter().enumerate() {
+        if *val == count + 1 {
+            inds[*val as usize] = ind;
+            count = *val;
+        }
+    }
+
+    for id in (0..=max).rev() {
+        let size = layout.iter().filter(|n| n == &&id).count();
+        for (i, w) in temp.clone().windows(size).enumerate() {
+            if *w == vec![-1; size] {
+                if i > inds[id as usize] {
+                    break;
+                }
+                for j in 0..size {
+                    temp.swap(i + j, inds[id as usize] + j);
+                }
+                break;
+            }
+        }
+    }
+
+    Some(checksum2(&temp))
 }
 
 #[cfg(test)]
@@ -94,6 +159,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
